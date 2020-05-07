80e92612cf18f036d77af3cafd095aad777c225c
Diulia Granatto <diuliagranatto@gmail.com>
2020-05-06 22:37:17 -0300

    Relatorio gerado

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ int main()
 {
     string sexo;
     int cervejas, refri, espetos;
+    double consumo, couvert, ingresso, total;
 
     cout << "Sexo: ";
     cin >> sexo;
@@ -17,6 +18,42 @@ int main()
     cout << "Quantidade de espetinhos: ";
     cin >> espetos;
 
+    if (sexo == "F") {
+
+        ingresso = 8.0;
+    }
+    else {
+
+        ingresso = 10.0;
+    }
+    consumo = cervejas * 5.0 + refri * 3.0 + espetos * 7.0;
+
+    if (consumo > 30.0){
+
+        couvert = 0.0;
+    }
+    else {
+
+        couvert = 4.0;
+    }
+
+    total = ingresso + consumo + couvert;
+
+    cout << fixed << setprecision(2);
+
+cout << endl << "RELATORIO:" << endl;
+cout << "Consumo = R$ " << consumo << endl;
+
+if (couvert == 0.0) {
+
+    cout << "Isento de Couvert" << endl;
+}
+else {
+
+    cout << "Couvert = R$ " << couvert << endl;
+}
+cout << "Ingresso = R$ " << ingresso << endl;
+cout << endl << "Valor a pagar = R$ " << total << endl;
 
     return 0;
 }
